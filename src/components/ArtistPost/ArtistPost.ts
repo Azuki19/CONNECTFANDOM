f9a1c7055ab744e0005c8b98b8a460bf25d995cc
CrisMarin1211 <alca81411@gmail.com>
2024-03-26 18:26:41 -0500

    feat - c

---
+++ src/components/ArtistPost/ArtistPost.ts
@@ -0,0 +1,79 @@
+import styles from './ArtistPost.css';
+
+export enum Attribute {
+	'uid' = 'uid',
+	'type' = 'type',
+	'name' = 'name',
+	'image' = 'image',
+	'username' = 'username',
+	'email' = 'email',
+	'password' = 'password',
+	'info' = 'info',
+	'followers' = 'followers',
+	'titlePost' = 'titlepost',
+	'infoPost' = 'infopost',
+	'imagePost' = 'imagepost',
+}
+
+class ArtistPost extends HTMLElement {
+	uid?: number;
+	type;
+	name?: string;
+	image?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+	info?: string;
+	followers?: number;
+	titlepost?: string;
+	infopost?: string;
+	imagepost?: string;
+
+	static get observedAttributes() {
+		return Object.values(Attribute);
+	}
+
+	attributeChangedCallback(propName: string, oldValue: string | null, newValue: string | null) {
+		if (newValue !== null) {
+			switch (propName) {
+				case Attribute.uid:
+				case Attribute.followers:
+					this[propName] = parseInt(newValue);
+					break;
+				default:
+					this[propName] = newValue;
+					break;
+			}
+		}
+		this.render();
+	}
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+        <section>
+          <div>
+             <img src="${this.image}" alt="">
+             <h1>${this.name}</h1>
+             <p>${this.username}</p>
+          </div>
+
+             <div>
+                <p>${this.titlepost}</p>
+                <p>${this.infopost}</p>
+                <p>${this.imagepost}</p>
+             </div>
+         </section>
+         `;
+		}
+	}
+}
