93c0be5b87c2abf4de0cf3bec5474a05e31f8f6f
CrisMarin1211 <alca81411@gmail.com>
2024-03-29 19:43:06 -0500

    feat - MainHeader - C

---
+++ src/components/ArtistPost/ArtistPost.ts
@@ -0,0 +1,85 @@
+import styles from './ArtistPost.css';
+
+export enum ArtistAttribute {
+	'uid' = 'uid',
+	'type' = 'type',
+	'name' = 'name',
+	'image' = 'image',
+	'username' = 'username',
+	'email' = 'email',
+	'password' = 'password',
+	'info' = 'info',
+	'followers' = 'followers',
+	'titlePost' = 'titlepost',
+	'infoPost' = 'infopost',
+	'imagePost' = 'imagepost',
+}
+
+class ArtistPost extends HTMLElement {
+	uid?: number;
+	type?: string;
+	name?: string;
+	image?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+	info?: string;
+	followers?: number;
+	titlepost?: string;
+	infopost?: string;
+	imagepost?: string;
+
+	static get observedAttributes() {
+		return Object.values(ArtistAttribute);
+	}
+
+	attributeChangedCallback(propName: string, oldValue: string | null, newValue: string | null) {
+		if (newValue !== null) {
+			switch (propName) {
+				case ArtistAttribute.uid:
+				case ArtistAttribute.followers:
+					this[propName] = parseInt(newValue);
+					break;
+				default:
+					this[propName] = newValue;
+					break;
+			}
+		}
+		this.render();
+	}
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+        <section>
+          <div>
+             <img src="${this.image}" alt="">
+             <h1>${this.name}</h1>
+             <p>${this.username}</p>
+          </div>
+
+          <div>
+              <p>${this.titlepost}</p>
+              <p>${this.infopost}</p>
+          </div>
+         </section>
+         `;
+		}
+
+		const cssArtistPost = this.ownerDocument.createElement('style');
+		cssArtistPost.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssArtistPost);
+	}
+}
+
+customElements.define('artist-post', ArtistPost);
+export default ArtistPost;
