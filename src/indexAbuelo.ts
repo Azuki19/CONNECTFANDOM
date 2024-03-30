16ecf8b5ce9294e102b80a62420a1b7a120978fe
Azuki19 <analucia27259@gmail.com>
2024-03-29 19:20:57 -0500

    fix - indexAbuelo

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -91,27 +91,51 @@ class AppContainer extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-			<header> CONNECT FANDOM </header>
+            <header> CONNECT FANDOM </header>
+						<div id="mini-header-container"></div>
+            <section id="artist-posts-section">
+            </section>
+						<div id="a">
+						<section id="create-post-section">
+            </section>
+            <section id="user-posts-section">
+            </section>
+						</div>
+            <section id="more-about-section">
+            </section>
+        `;
 
-            `;
+			const miniHeaderContainer = this.shadowRoot.getElementById('mini-header-container');
+			miniHeaderContainer.appendChild(new components.MiniHeader());
 
-			this.shadowRoot?.appendChild(new components.MiniHeader());
+			const artistPostsSection = this.shadowRoot.getElementById('artist-posts-section');
+			const userPostsSection = this.shadowRoot.getElementById('user-posts-section');
+			const moreAboutSection = this.shadowRoot.getElementById('more-about-section');
+			const createPostSection = this.shadowRoot.getElementById('create-post-section');
+
+			//Agarro las secciones del html, las volvio constantes o cajitas.Luego cojio los componentes de maaaaas arriba y las metió en estas cajitas
 
 			this.artistpost.forEach((artistpost) => {
-				this.shadowRoot?.appendChild(artistpost);
+				artistPostsSection.appendChild(artistpost);
 			});
 
 			this.userpost.forEach((userpost) => {
-				this.shadowRoot?.appendChild(userpost);
+				userPostsSection.appendChild(userpost);
 			});
 
 			this.moreabout.forEach((moreabout) => {
-				this.shadowRoot?.appendChild(moreabout);
+				moreAboutSection.appendChild(moreabout);
 			});
 
 			this.createpost.forEach((createpost) => {
-				this.shadowRoot?.appendChild(createpost);
+				createPostSection.appendChild(createpost);
 			});
+
+			const cssAbuelo = this.ownerDocument.createElement('style');
+			cssAbuelo.innerHTML = style;
+			this.shadowRoot?.appendChild(cssAbuelo);
 		}
 	}
 }
+
+customElements.define('app-container', AppContainer);
