71349846847bc2008c84464662650dfd5dbcd96c
Azuki19 <analucia27259@gmail.com>
2024-05-01 12:35:57 -0500

    feat

---
+++ src/screens/aboutTheBand.ts
@@ -0,0 +1,9 @@
+import * as components from '../components/indexPadre';
+import { bandsdata } from '../data/bandsData';
+import style from './aboutTheBand.css';
+class AppDashboard extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+}
