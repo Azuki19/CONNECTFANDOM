404fdf2502b9bddc5d8a794dc9ecce34bd61dd53
Azuki19 <analucia27259@gmail.com>
2024-05-01 23:20:55 -0500

    feat - indexPadre

--- src/components/indexPadre.ts
+++ src/components/indexPadre.ts
@@ -1,4 +1,6 @@
 export { default as BandGallery } from './BandGallery/BandGallery';
+export { default as Members } from './Members/Members';
+export { default as BandInfo } from './BandInfo/BandInfo';
 export { default as UserPost } from './UserPost/UserPost';
 export { default as ArtistPost } from './ArtistPost/ArtistPost';
 export { default as MoreAbout } from './MoreAbout/MoreAbout';
