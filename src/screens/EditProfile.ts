960447baace2a852b997137a33d43854c323b5c8
CrisMarin1211 <alca81411@gmail.com>
2024-05-02 12:54:26 -0500

    feat - screensEP - C

--- src/screens/EditProfile.ts
+++ src/screens/EditProfile.ts
@@ -2,6 +2,4 @@ import * as components from '../components/indexPadre';
 import { bandsdata } from '../data/bandsData';
 import { MyChemicalRomanceData } from '../data/MyChemicalRomanceData';
 import { EditProfile } from '../components/indexPadre';
-import { Members } from '../components/indexPadre';
-import { BandInfo } from '../components/indexPadre';
 import style from './aboutTheBand.css';
