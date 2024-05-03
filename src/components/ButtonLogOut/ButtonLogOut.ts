bb071b6cedf1130038c84c1fbabad1ef862bca13
CrisMarin1211 <alca81411@gmail.com>
2024-05-03 14:51:22 -0500

    feat - ButtonLogOut - C

---
+++ src/components/ButtonLogOut/ButtonLogOut.ts
@@ -0,0 +1,32 @@
+import styles from './ButtonLogOut.css';
+
+class ButtonLogOut extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+
+			<section class='Button-Log-Out'>
+        <div class='div1'>
+        <button class='Log-Out'>LOG OUT</button>
+        </div>
+			</section>
+
+		`;
+		}
+		const cssLogOut = this.ownerDocument.createElement('style');
+		cssLogOut.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssLogOut);
+	}
+}
+
+customElements.define('Button-LogOut', ButtonLogOut);
+export default ButtonLogOut;
