115696d8add44c70eb1f5e4be04b616cf1955a89
Azuki19 <analucia27259@gmail.com>
2024-05-07 23:11:11 -0500

    feat - index padre screens

---
+++ src/screens/indexPadreScreens.ts
@@ -0,0 +1,5 @@
+import { default as AppEditProfile } from './EditProfilee';
+import { default as AppAboutTheBand } from './aboutTheBand';
+import { default as AppDashboard } from './dashboard';
+import { default as AppArtistDashboard } from './artistDashboard';
+import { default as ProfileDashboard } from './profile';
