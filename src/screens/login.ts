39c4300190f49a0abb7fd3d7e62cbb066dba6bdd
Azuki19 <analucia27259@gmail.com>
2024-05-09 03:20:09 -0500

    feat - login

---
+++ src/screens/login.ts
@@ -0,0 +1,45 @@
+import * as components from '../components/indexPadre';
+import style from './login.css';
+
+class AppLogin extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+			<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+			<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+			<section id='background'>
+      <section id='container'>
+				<section id='image-login-container'></section>
+        <section id='login-form-container'></section>
+        </section>
+			</section>
+        `;
+
+			const LoginFormContainer = this.shadowRoot.getElementById('login-form-container');
+			LoginFormContainer.appendChild(new components.LoginForm());
+
+			const ImageLoginContainer = this.shadowRoot.getElementById('image-login-container');
+			ImageLoginContainer.appendChild(new components.ImageLogin());
+
+			const cssAbuelo = this.ownerDocument.createElement('style');
+			cssAbuelo.innerHTML = style;
+			this.shadowRoot?.appendChild(cssAbuelo);
+		}
+	}
+}
+
+customElements.define('app-login', AppLogin);
+export default AppLogin;
