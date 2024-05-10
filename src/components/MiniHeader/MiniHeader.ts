4b8371e2dd09a407755adb638d818017a7ab57bc
Azuki19 <analucia27259@gmail.com>
2024-05-10 00:31:50 -0500

    featt - MiniHeader

--- src/components/MiniHeader/MiniHeader.ts
+++ src/components/MiniHeader/MiniHeader.ts
@@ -7,33 +7,29 @@ class MiniHeader extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-
 		this.onButtonClicked = this.onButtonClicked.bind(this);
-		this.addListeners();
+		addObserver(this);
 	}
 
 	connectedCallback() {
-		this.mount();
-	}
-
-	mount() {
 		this.render();
+		this.addListeners();
 	}
 
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-					<section class='sectionminiheader'>
-							<h4 id="feed">FEED</h4>
-							<h4 id="artists">ARTISTS</h4>
-							<h4 id="events">EVENTS</h4>
-							<h4 id="about-the-band">ABOUT THE BAND</h4>
-					</section>
-			`;
+							<section class='sectionminiheader'>
+									<h4 id="feed">FEED</h4>
+									<h4 id="artists">ARTISTS</h4>
+									<h4 id="events">EVENTS</h4>
+									<h4 id="about-the-band">ABOUT THE BAND</h4>
+							</section>
+					`;
+			const cssMiniHeader = this.ownerDocument.createElement('style');
+			cssMiniHeader.innerHTML = styles;
+			this.shadowRoot.appendChild(cssMiniHeader);
 		}
-		const cssMiniHeader = this.ownerDocument.createElement('style');
-		cssMiniHeader.innerHTML = styles;
-		this.shadowRoot?.appendChild(cssMiniHeader);
 	}
 
 	addListeners() {
@@ -45,22 +41,17 @@ class MiniHeader extends HTMLElement {
 	onButtonClicked(event) {
 		const target = event.target.getAttribute('id');
 		console.log('Clicked on:', target);
-		// Aquí puedes definir las acciones correspondientes para cada identificador
 		switch (target) {
 			case 'feed':
-				// Acción para FEED
 				dispatch(navigate('DASHBOARD'));
 				break;
 			case 'artists':
-				// Acción para ARTISTS
 				dispatch(navigate('ARTISTDASHBOARD'));
 				break;
 			case 'events':
-				// Acción para EVENTS
-				dispatch(navigate(''));
+				dispatch(navigate('EVENTS'));
 				break;
 			case 'about-the-band':
-				// Acción para ABOUT THE BAND
 				dispatch(navigate('ABOUTTHEBAND'));
 				break;
 			default:
