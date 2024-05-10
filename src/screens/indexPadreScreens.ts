8a66754c54fb998249da5cbb5067553cc209c653
Azuki19 <analucia27259@gmail.com>
2024-05-10 15:46:41 -0500

    feat - indexpadrescreens

--- src/screens/indexPadreScreens.ts
+++ src/screens/indexPadreScreens.ts
@@ -5,4 +5,4 @@ export { default as AppArtistDashboard } from './artistDashboard';
 export { default as ProfileDashboard } from './profile';
 export { default as Applogin } from './login';
 export { default as AppRegister } from './register';
-// export { default as AppBandEvents } from './BandEvents';
+export { default as AppBandEvents } from './BandEvents';
