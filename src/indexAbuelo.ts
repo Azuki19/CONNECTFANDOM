57abd598af3ac649c00632a9d17ccdf01f4bd60b
Azuki19 <analucia27259@gmail.com>
2024-05-09 21:18:28 -0500

    fix - indexAbuelo

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -1,5 +1,11 @@
 import './screens/login';
 import './components/indexPadre';
+import './screens/indexPadreScreens';
+import { appState } from './store';
+import AppDashboard from './screens/dashboard';
+import AppLogin from './screens/login';
+import { AppAboutTheBand, AppEditProfile, ProfileDashboard } from './screens/indexPadreScreens';
+import { AppArtistDashboard } from './screens/indexPadreScreens';
 
 class AppContainer extends HTMLElement {
 	constructor() {
@@ -12,8 +18,43 @@ class AppContainer extends HTMLElement {
 	}
 
 	render() {
-		const something = this.ownerDocument.createElement('app-login');
-		this.shadowRoot?.appendChild(something);
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = ``;
+		}
+		switch (appState.screen) {
+			case 'LOGIN':
+				const login = this.ownerDocument.createElement('app-login') as AppLogin;
+				this.shadowRoot?.appendChild(login);
+				break;
+
+			case 'DASHBOARD':
+				const dashboard = this.ownerDocument.createElement('app-dashboard') as AppDashboard;
+				this.shadowRoot?.appendChild(dashboard);
+				break;
+
+			case 'ABOUTTHEBAND':
+				const aboutTheBand = this.ownerDocument.createElement('app-about-the-band') as AppAboutTheBand;
+				this.shadowRoot?.appendChild(aboutTheBand);
+				break;
+
+			case 'ARTISTDASHBOARD':
+				const artistdashboard = this.ownerDocument.createElement('app-artist-dashboard') as AppArtistDashboard;
+				this.shadowRoot?.appendChild(artistdashboard);
+				break;
+
+			case 'EDITPROFILE':
+				const EditProfile = this.ownerDocument.createElement('app-edit-profile') as AppEditProfile;
+				this.shadowRoot?.appendChild(EditProfile);
+				break;
+
+			case 'PROFILE':
+				const profile = this.ownerDocument.createElement('profile-dashboard') as ProfileDashboard;
+				this.shadowRoot?.appendChild(profile);
+				break;
+
+			default:
+				break;
+		}
 	}
 }
 
