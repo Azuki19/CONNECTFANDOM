287fa7bcb9e6e311e64e84cb859307afa29e2d1f
CrisMarin1211 <alca81411@gmail.com>
2024-05-10 17:03:24 -0500

    feat - C

---
+++ src/components/Register/RegisterForm.ts
@@ -0,0 +1,137 @@
+import styles from './RegisterForm.css';
+import * as components from '..//indexPadre';
+
+import { dispatch } from '../../store';
+import { navigate } from '../../store/action';
+import { addObserver } from '../../store';
+
+class RegisterForm extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+
+		this.onButtonClicked = this.onButtonClicked.bind(this);
+		addObserver(this);
+	}
+
+	connectedCallback() {
+		this.mount();
+	}
+
+	mount() {
+		this.render();
+		this.addListeners();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = ``;
+		}
+		const generalDiv = document.createElement('div');
+		generalDiv.id = 'general';
+
+		// Contenedor para los textos
+		const textContainer = document.createElement('div');
+		textContainer.id = 'text-container';
+
+		// Texto 1: Welcome to
+		const welcomeText = document.createElement('span');
+		welcomeText.textContent = 'Welcome to ';
+
+		// Texto 2: CONNECT FANDOM!
+		const fandomText = document.createElement('span');
+		fandomText.textContent = 'CONNECT FANDOM!';
+
+		// Agregar textos al contenedor
+		textContainer.appendChild(welcomeText);
+		textContainer.appendChild(fandomText);
+
+		// Create the inputs div
+		const inputsDiv = document.createElement('div');
+		inputsDiv.id = 'inputs';
+
+		// Create name input
+		const nameInput = document.createElement('input');
+		nameInput.type = 'text';
+		nameInput.placeholder = 'Name';
+		nameInput.classList.add('name');
+		nameInput.id = 'innnput';
+
+		// Create username input
+		const usernameInput = document.createElement('input');
+		usernameInput.type = 'text';
+		usernameInput.placeholder = 'Username';
+		usernameInput.classList.add('username');
+		usernameInput.id = 'innnput';
+
+		const emailInput = document.createElement('input');
+		emailInput.type = 'email';
+		emailInput.placeholder = 'Email';
+		emailInput.classList.add('email');
+		emailInput.id = 'innnput';
+
+		const passwordInput = document.createElement('input');
+		passwordInput.type = 'password';
+		passwordInput.placeholder = 'Password';
+		passwordInput.id = 'password';
+		passwordInput.id = 'innnput';
+
+		// Create the login button
+		const loginButton = document.createElement('section');
+		loginButton.classList.add('button');
+		loginButton.appendChild(new components.ButtonRegister());
+
+		// Create the 'down-part' div
+		const downPartDiv = document.createElement('div');
+		downPartDiv.id = 'down-part';
+
+		// Append the elements to build the structure
+		generalDiv.appendChild(textContainer); // Agregar contenedor de texto
+
+		inputsDiv.appendChild(nameInput);
+		inputsDiv.appendChild(usernameInput);
+		inputsDiv.appendChild(emailInput);
+		inputsDiv.appendChild(passwordInput);
+		inputsDiv.appendChild(loginButton);
+
+		// Append the 'Remember your password' checkbox and 'Forgot password' link
+		generalDiv.appendChild(inputsDiv);
+		generalDiv.appendChild(downPartDiv);
+		const notFandomDiv = document.createElement('div');
+		notFandomDiv.id = 'not-fandom-div';
+		const InnerDiv = document.createElement('div');
+		InnerDiv.id = 'inner-div';
+		// Create the 'Not a part of Connect Fandom' text
+		const notFandomText = document.createElement('span');
+		notFandomText.textContent = '¿Already have an account?';
+		notFandomText.id = 'not-fandom';
+
+		// Create the 'Sign in' text
+		const signInText = document.createElement('span');
+		signInText.textContent = 'Log in';
+		signInText.id = 'sign-in-text';
+
+		InnerDiv.appendChild(notFandomText);
+		InnerDiv.appendChild(signInText);
+
+		notFandomDiv.appendChild(InnerDiv);
+		generalDiv.appendChild(notFandomDiv);
+
+		this.shadowRoot?.appendChild(generalDiv);
+
+		const cssMiniHeader = this.ownerDocument.createElement('style');
+		cssMiniHeader.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssMiniHeader);
+	}
+	addListeners() {
+		this.shadowRoot.querySelector('#sign-in-text')?.addEventListener('click', this.onButtonClicked);
+	}
+
+	onButtonClicked() {
+		console.log('holaaa');
+		dispatch(navigate('LOGIN'));
+	}
+}
+
+customElements.define('register-form', RegisterForm);
+export default RegisterForm;
