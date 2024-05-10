aec276a2d9335d0cb96608991b5d92713ce53a66
CrisMarin1211 <alca81411@gmail.com>
2024-05-10 17:03:27 -0500

    feat - C

--- src/data/bandsData.ts
+++ src/data/bandsData.ts
@@ -1,110 +1,110 @@
-interface DataShape {
-	id: number;
-	bandName: string;
-	bandImage: string;
-	bandInfo: string;
-	bandGallery: {
-		photo1: string;
-		photo2: string;
-		photo3: string;
-		photo4: string;
-		photo5: string;
-		photo6: string;
-		photo7: string;
-	};
-	events?: {
-		event1?: {
-			eventName: string;
-			slideImage: string;
-			infoImage: string;
-			infoEvents: string;
-			dateEvents: string;
-		};
-		event2?: {
-			eventName: string;
-			slideImage: string;
-			infoImage: string;
-			infoEvents: string;
-			dateEvents: string;
-		};
-		event3?: {
-			eventName: string;
-			slideImage: string;
-			infoImage: string;
-			infoEvents: string;
-			dateEvents: string;
-		};
-	};
-}
-export const bandsdata: DataShape[] = [
-	{
-		id: 1,
-		bandName: 'MY CHEMICAL ROMANCE',
-		bandImage: 'https://pbs.twimg.com/media/GKGEoDiXgAApA5N?format=png&name=small',
-		bandInfo:
-			'Formed in NJ, My Chemical Romance made its debut in 2002 with the independently released album I Brought You My Bullets, You Brought Me Your Love. The band signed to Reprise Records the following year and made its major label debut with 2004’s Three Cheers for Sweet Revenge, now 3x certified Platinum. The album contained the Platinum hit "Im Not Okay (I Promise)," the Gold-certified "Helena," and "The Ghost of You." Rolling Stone hailed the 3x Platinum The Black Parade as one of the top albums of 2006.Lead single “Welcome to the Black Parade” topped both Billboard’s Alternative Songs tally and the UK Official Singles chart and is now 3x Platinum.',
-		bandGallery: {
-			photo1: 'https://www.billboard.com/wp-content/uploads/stylus/500264-my_chemical_romance_617_409.jpg?w=617',
-			photo2: 'https://www.billboard.com/wp-content/uploads/media/my-chemical-romance-2011-xbv-billboard-1548.jpg',
-			photo3:
-				'https://punkeando.com.mx/wp-content/webpc-passthru.php?src=https://punkeando.com.mx/wp-content/uploads/2022/05/my-chemical-romance-top-10.jpg&nocache=1',
-			photo4:
-				'https://portalpopline.com.br/wp-content/uploads/2022/05/my-chemical-romance-The-Foundations-of-Decay-single.jpg',
-			photo5:
-				'https://media2.citybeat.com/citybeat/imager/mcr/u/zoom/13752614/mychemicalromance_brittanythornton_07.png?cb=1693337128',
-			photo6:
-				'https://ca-times.brightspotcdn.com/dims4/default/6988f0a/2147483647/strip/true/crop/600x400+0+0/resize/1200x800!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F78%2F93%2F92b6a06f78a8c0f3c57f45272116%2Fla-162257-ca-1121-mychemicalromance-220101121',
-			photo7:
-				'https://qtxasset.com/cdn-cgi/image/w=850,h=478,g=0.5x0.5,f=auto,fit=crop/https://qtxasset.com/quartz/qcloud4/2022-11/MCR-JVO-00009-1000.jpg?VersionId=JfuLR54G6M.xFN23KcJHL7vT2P41NnBO',
-		},
+// interface DataShape {
+// 	id: number;
+// 	bandName: string;
+// 	bandImage: string;
+// 	bandInfo: string;
+// 	bandGallery: {
+// 		photo1: string;
+// 		photo2: string;
+// 		photo3: string;
+// 		photo4: string;
+// 		photo5: string;
+// 		photo6: string;
+// 		photo7: string;
+// 	};
+// 	events?: {
+// 		event1?: {
+// 			eventName: string;
+// 			slideImage: string;
+// 			infoImage: string;
+// 			infoEvents: string;
+// 			dateEvents: string;
+// 		};
+// 		event2?: {
+// 			eventName: string;
+// 			slideImage: string;
+// 			infoImage: string;
+// 			infoEvents: string;
+// 			dateEvents: string;
+// 		};
+// 		event3?: {
+// 			eventName: string;
+// 			slideImage: string;
+// 			infoImage: string;
+// 			infoEvents: string;
+// 			dateEvents: string;
+// 		};
+// 	};
+// }
+// export const bandsdata: DataShape[] = [
+// 	{
+// 		id: 1,
+// 		bandName: 'MY CHEMICAL ROMANCE',
+// 		bandImage: 'https://pbs.twimg.com/media/GKGEoDiXgAApA5N?format=png&name=small',
+// 		bandInfo:
+// 			'Formed in NJ, My Chemical Romance made its debut in 2002 with the independently released album I Brought You My Bullets, You Brought Me Your Love. The band signed to Reprise Records the following year and made its major label debut with 2004’s Three Cheers for Sweet Revenge, now 3x certified Platinum. The album contained the Platinum hit "Im Not Okay (I Promise)," the Gold-certified "Helena," and "The Ghost of You." Rolling Stone hailed the 3x Platinum The Black Parade as one of the top albums of 2006.Lead single “Welcome to the Black Parade” topped both Billboard’s Alternative Songs tally and the UK Official Singles chart and is now 3x Platinum.',
+// 		bandGallery: {
+// 			photo1: 'https://www.billboard.com/wp-content/uploads/stylus/500264-my_chemical_romance_617_409.jpg?w=617',
+// 			photo2: 'https://www.billboard.com/wp-content/uploads/media/my-chemical-romance-2011-xbv-billboard-1548.jpg',
+// 			photo3:
+// 				'https://punkeando.com.mx/wp-content/webpc-passthru.php?src=https://punkeando.com.mx/wp-content/uploads/2022/05/my-chemical-romance-top-10.jpg&nocache=1',
+// 			photo4:
+// 				'https://portalpopline.com.br/wp-content/uploads/2022/05/my-chemical-romance-The-Foundations-of-Decay-single.jpg',
+// 			photo5:
+// 				'https://media2.citybeat.com/citybeat/imager/mcr/u/zoom/13752614/mychemicalromance_brittanythornton_07.png?cb=1693337128',
+// 			photo6:
+// 				'https://ca-times.brightspotcdn.com/dims4/default/6988f0a/2147483647/strip/true/crop/600x400+0+0/resize/1200x800!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F78%2F93%2F92b6a06f78a8c0f3c57f45272116%2Fla-162257-ca-1121-mychemicalromance-220101121',
+// 			photo7:
+// 				'https://qtxasset.com/cdn-cgi/image/w=850,h=478,g=0.5x0.5,f=auto,fit=crop/https://qtxasset.com/quartz/qcloud4/2022-11/MCR-JVO-00009-1000.jpg?VersionId=JfuLR54G6M.xFN23KcJHL7vT2P41NnBO',
+// 		},
 
-		events: {
-			event1: {
-				eventName: 'WHEN WE WERE YOUNG',
-				slideImage:
-					'https://assets-global.website-files.com/61d462885e62e9104aba522b/65a82fe24bf269615f89182e_WWWY%2024%20S%20(1).jpg',
-				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
-				infoEvents:
-					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
-				dateEvents: 'October 19 & 20, 2024',
-			},
+// 		events: {
+// 			event1: {
+// 				eventName: 'WHEN WE WERE YOUNG',
+// 				slideImage:
+// 					'https://assets-global.website-files.com/61d462885e62e9104aba522b/65a82fe24bf269615f89182e_WWWY%2024%20S%20(1).jpg',
+// 				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
+// 				infoEvents:
+// 					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
+// 				dateEvents: 'October 19 & 20, 2024',
+// 			},
 
-			event2: {
-				eventName: 'MY CHEMICAL ROMANCE',
-				slideImage: 'https://pbs.twimg.com/media/FE6fvOHWYAc5Vpb?format=jpg&name=large',
-				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
-				infoEvents:
-					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
-				dateEvents: 'March 11 - 19, 2023',
-			},
+// 			event2: {
+// 				eventName: 'MY CHEMICAL ROMANCE',
+// 				slideImage: 'https://pbs.twimg.com/media/FE6fvOHWYAc5Vpb?format=jpg&name=large',
+// 				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
+// 				infoEvents:
+// 					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
+// 				dateEvents: 'March 11 - 19, 2023',
+// 			},
 
-			event3: {
-				eventName: 'MY CHEMICAL ROMANCE "WITH VERY SPECIAL GUESTS: GAYLE - STARCRAWLER"',
-				slideImage: 'https://lastfm.freetls.fastly.net/i/u/ar0/2f7765c235f73331399508d85d24e38e',
-				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
-				infoEvents:
-					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
-				dateEvents: 'May 24 & 25, 2022',
-			},
-		},
-	},
-	{
-		id: 2,
-		bandName: 'PIERCE THE VEIL',
-		bandImage: 'https://store.fearlessrecords.com/cdn/shop/collections/piercetheveil.jpg?v=1662006994',
-		bandInfo:
-			'Pierce The Veil es una banda estadounidense de post-hardcore formada en el año 1998 bajo el nombre de Early Times, en el 2004 renombrada Before Today, y tras un pequeño hiato en el 2006, la banda oficialmente pasó a llamarse Pierce The Veil, bajo este nombre la banda ha lanzado cinco álbumes de estudio, A Flair for the Dramatic en junio del 2007, Selfish Machines en junio del 2010, Collide with the Sky en julio de 2012, Misadventures en mayo de 2016 y The Jaws Of Life el 10 de febrero de 2023.',
-		bandGallery: {
-			photo1: 'https://tracktohell.com/wp-content/uploads/2020/04/PTV730.jpg',
-			photo2:
-				'https://cms.kerrang.com/images/_1200x630_crop_center-center_82_none/My-Chemical-Romance-Danger-Days-promo-3.jpg?mtime=1573469727https://fearlessrecords.com/wp-content/uploads/2023/02/W1A0301-scaled-e1676010334222.jpg',
-			photo3: 'https://www.mondosonoro.com/wp-content/uploads/2016/05/piercetheveil.jpg',
-			photo4: 'https://setlist.me/wp-content/uploads/2023/03/DSC08403-2.jpg',
-			photo5:
-				'https://i0.wp.com/expectador.cl/wp-content/uploads/2023/04/Diseno-sin-titulo-3.png?resize=1020%2C600&ssl=1',
-			photo6: 'https://www.metalmusicarchives.com/images/artists/pierce-the-veil-20190506143001.jpg',
-			photo7:
-				'https://punkeando.com.mx/wp-content/webpc-passthru.php?src=https://punkeando.com.mx/wp-content/uploads/2023/03/Pierce-The-Veil-Carpa-Velodromo-CDMX-2023.jpg&nocache=1',
-		},
-	},
-];
+// 			event3: {
+// 				eventName: 'MY CHEMICAL ROMANCE "WITH VERY SPECIAL GUESTS: GAYLE - STARCRAWLER"',
+// 				slideImage: 'https://lastfm.freetls.fastly.net/i/u/ar0/2f7765c235f73331399508d85d24e38e',
+// 				infoImage: 'https://s1.ticketm.net/dam/a/23b/99d97755-48ac-4438-90df-2f9a1e3f523b_RETINA_PORTRAIT_16_9.jpg',
+// 				infoEvents:
+// 					'Each General Admission ticket includes admittance to Las Vegas Festival Grounds for When We Were Young on October 20, 2024, plus the following: - Performances all day on multiple stages at Las Vegas Festival Grounds. - Food choices for purchase from regional and local vendors. -Bars, concessions, official band merch, festival merch, free water stations, and more. All tiers are the same ticket type. Please select the lowest tier for the best available price. Prices include all fees. Any applicable taxes and shipping will be added at checkout. Tickets are first come, first served, not guaranteed.',
+// 				dateEvents: 'May 24 & 25, 2022',
+// 			},
+// 		},
+// 	},
+// 	{
+// 		id: 2,
+// 		bandName: 'PIERCE THE VEIL',
+// 		bandImage: 'https://store.fearlessrecords.com/cdn/shop/collections/piercetheveil.jpg?v=1662006994',
+// 		bandInfo:
+// 			'Pierce The Veil es una banda estadounidense de post-hardcore formada en el año 1998 bajo el nombre de Early Times, en el 2004 renombrada Before Today, y tras un pequeño hiato en el 2006, la banda oficialmente pasó a llamarse Pierce The Veil, bajo este nombre la banda ha lanzado cinco álbumes de estudio, A Flair for the Dramatic en junio del 2007, Selfish Machines en junio del 2010, Collide with the Sky en julio de 2012, Misadventures en mayo de 2016 y The Jaws Of Life el 10 de febrero de 2023.',
+// 		bandGallery: {
+// 			photo1: 'https://tracktohell.com/wp-content/uploads/2020/04/PTV730.jpg',
+// 			photo2:
+// 				'https://cms.kerrang.com/images/_1200x630_crop_center-center_82_none/My-Chemical-Romance-Danger-Days-promo-3.jpg?mtime=1573469727https://fearlessrecords.com/wp-content/uploads/2023/02/W1A0301-scaled-e1676010334222.jpg',
+// 			photo3: 'https://www.mondosonoro.com/wp-content/uploads/2016/05/piercetheveil.jpg',
+// 			photo4: 'https://setlist.me/wp-content/uploads/2023/03/DSC08403-2.jpg',
+// 			photo5:
+// 				'https://i0.wp.com/expectador.cl/wp-content/uploads/2023/04/Diseno-sin-titulo-3.png?resize=1020%2C600&ssl=1',
+// 			photo6: 'https://www.metalmusicarchives.com/images/artists/pierce-the-veil-20190506143001.jpg',
+// 			photo7:
+// 				'https://punkeando.com.mx/wp-content/webpc-passthru.php?src=https://punkeando.com.mx/wp-content/uploads/2023/03/Pierce-The-Veil-Carpa-Velodromo-CDMX-2023.jpg&nocache=1',
+// 		},
+// 	},
+// ];
