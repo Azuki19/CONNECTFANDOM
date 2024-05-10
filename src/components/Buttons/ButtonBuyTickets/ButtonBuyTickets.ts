6ec76878633bcfdb257d961dcdf7fc05da385866
CrisMarin1211 <alca81411@gmail.com>
2024-05-10 17:00:51 -0500

    feat - C

---
+++ src/components/Buttons/ButtonBuyTickets/ButtonBuyTickets.ts
@@ -0,0 +1,49 @@
+import styles from './ButtonBuyTickets.css';
+
+import { dispatch } from '../../../store';
+import { navigate } from '../../../store/action';
+import { addObserver } from '../../../store';
+
+class ButtonBuyTickets extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+
+		this.onButtonClicked = this.onButtonClicked.bind(this);
+		addObserver(this);
+	}
+
+	connectedCallback() {
+		this.mount();
+	}
+
+	mount() {
+		this.render();
+		this.addListeners();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+				<section class="Button-Send">
+					<button class="Send">LOG OUT</button>
+				</section>
+			`;
+		}
+
+		const cssLogOut = this.ownerDocument.createElement('style');
+		cssLogOut.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssLogOut);
+	}
+	addListeners() {
+		this.shadowRoot.querySelector('.Button-Send')?.addEventListener('click', this.onButtonClicked);
+	}
+
+	onButtonClicked() {
+		console.log('holaaa');
+		dispatch(navigate('DASHBOARD'));
+	}
+}
+
+customElements.define('button-send', ButtonSend);
+export default ButtonSend;
