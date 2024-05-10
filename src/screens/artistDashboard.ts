2a1bb63af50f9c709d9d8283c2de6961d63978fd
CrisMarin1211 <alca81411@gmail.com>
2024-05-10 11:53:20 -0500

    feat - artisDashboard - C

--- src/screens/artistDashboard.ts
+++ src/screens/artistDashboard.ts
@@ -1,10 +1,9 @@
 import * as components from '../components/indexPadre';
 import MainArtistPost, { Attribute } from '../components/MainArtistPost/MainArtistPost';
 import MoreAbout, { MoreAboutAttribute } from '../components/MoreAbout/MoreAbout';
-import { MyChemicalRomanceData } from '../data/MyChemicalRomanceData';
-import { bandsdata } from '../data/bandsData';
 import style from './artistDashboard.css';
 import MiniProfile, { MiniProfileAttribute } from '../components/miniProfile/miniProfile';
+import { getPosts, getBands } from '../utils/firebase';
 
 class AppArtistDashboard extends HTMLElement {
 	mainartistpost: MainArtistPost[] = [];
@@ -14,56 +13,64 @@ class AppArtistDashboard extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-
-		const UserData = MyChemicalRomanceData.filter((user) => user.type === 'Artist');
-		UserData.forEach((user) => {
-			const UserPostCard = this.ownerDocument.createElement('main-artist-post') as MainArtistPost;
-
-			UserPostCard.setAttribute(Attribute.uid, String(user.id));
-			UserPostCard.setAttribute(Attribute.type, user.type);
-			UserPostCard.setAttribute(Attribute.name, user.name);
-			UserPostCard.setAttribute(Attribute.image, user.image);
-			UserPostCard.setAttribute(Attribute.username, user.username);
-			UserPostCard.setAttribute(Attribute.titlePost, user.posts.post1.titlePost);
-			UserPostCard.setAttribute(Attribute.infoPost, user.posts.post1.infoPost);
-			UserPostCard.setAttribute(Attribute.imagePost, user.posts.post1.imagePost);
-
-			if (user.posts.post2) {
-				UserPostCard.setAttribute(Attribute.titlePost2, user.posts.post2.titlePost2);
-				UserPostCard.setAttribute(Attribute.infoPost2, user.posts.post2.infoPost2);
-				UserPostCard.setAttribute(Attribute.imagePost2, user.posts.post2.imagePost2);
-			}
-
-			this.mainartistpost.push(UserPostCard);
-		});
-
-		const miniData = MyChemicalRomanceData.filter((mini) => mini.id === 1);
-		miniData.forEach((mini) => {
-			const miniCard = this.ownerDocument.createElement('mini-profile') as MiniProfile;
-
-			miniCard.setAttribute(MiniProfileAttribute.uid, String(mini.id));
-			miniCard.setAttribute(MiniProfileAttribute.username, mini.username);
-			miniCard.setAttribute(MiniProfileAttribute.name, mini.name);
-			miniCard.setAttribute(MiniProfileAttribute.image, mini.image);
-			miniCard.setAttribute(MiniProfileAttribute.followers, String(mini.followers));
-
-			this.miniprofile.push(miniCard);
-		});
-
-		const BannerBand = bandsdata.find((band) => band.id === 1);
-
-		if (BannerBand) {
-			const MoreAboutCard = this.ownerDocument.createElement('more-about') as MoreAbout;
-
-			MoreAboutCard.setAttribute(MoreAboutAttribute.uid, String(BannerBand.id));
-			MoreAboutCard.setAttribute(MoreAboutAttribute.bandName, BannerBand.bandName);
-			MoreAboutCard.setAttribute(MoreAboutAttribute.bandImage, BannerBand.bandImage);
-
-			this.moreabout.push(MoreAboutCard);
-		}
+		this.initializeData();
 	}
-	connectedCallback() {
-		this.render();
+
+	async initializeData() {
+		try {
+			const [posts, bands] = await Promise.all([getPosts(), getBands()]);
+
+			const UserData = posts.filter((user) => user.type === 'Artist');
+			UserData.forEach((user) => {
+				const UserPostCard = this.ownerDocument.createElement('main-artist-post') as MainArtistPost;
+
+				UserPostCard.setAttribute(Attribute.uid, String(user.id));
+				UserPostCard.setAttribute(Attribute.type, user.type);
+				UserPostCard.setAttribute(Attribute.name, user.name);
+				UserPostCard.setAttribute(Attribute.image, user.image);
+				UserPostCard.setAttribute(Attribute.username, user.username);
+				UserPostCard.setAttribute(Attribute.titlePost, user.posts.post1.titlePost);
+				UserPostCard.setAttribute(Attribute.infoPost, user.posts.post1.infoPost);
+				UserPostCard.setAttribute(Attribute.imagePost, user.posts.post1.imagePost);
+
+				if (user.posts.post2) {
+					UserPostCard.setAttribute(Attribute.titlePost2, user.posts.post2.titlePost2);
+					UserPostCard.setAttribute(Attribute.infoPost2, user.posts.post2.infoPost2);
+					UserPostCard.setAttribute(Attribute.imagePost2, user.posts.post2.imagePost2);
+				}
+
+				this.mainartistpost.push(UserPostCard);
+			});
+
+			const miniData = posts.filter((mini) => mini.id === 1);
+			miniData.forEach((mini) => {
+				const miniCard = this.ownerDocument.createElement('mini-profile') as MiniProfile;
+
+				miniCard.setAttribute(MiniProfileAttribute.uid, String(mini.id));
+				miniCard.setAttribute(MiniProfileAttribute.username, mini.username);
+				miniCard.setAttribute(MiniProfileAttribute.name, mini.name);
+				miniCard.setAttribute(MiniProfileAttribute.image, mini.image);
+				miniCard.setAttribute(MiniProfileAttribute.followers, String(mini.followers));
+
+				this.miniprofile.push(miniCard);
+			});
+
+			const BannerBand = bands.find((band) => band.id === 1);
+
+			if (BannerBand) {
+				const MoreAboutCard = this.ownerDocument.createElement('more-about') as MoreAbout;
+
+				MoreAboutCard.setAttribute(MoreAboutAttribute.uid, String(BannerBand.id));
+				MoreAboutCard.setAttribute(MoreAboutAttribute.bandName, BannerBand.bandName);
+				MoreAboutCard.setAttribute(MoreAboutAttribute.bandImage, BannerBand.bandImage);
+
+				this.moreabout.push(MoreAboutCard);
+			}
+			// Llama a render() al final de initializeData()
+			this.render();
+		} catch (error) {
+			console.error('Error fetching data from Firebase:', error);
+		}
 	}
 
 	render() {
