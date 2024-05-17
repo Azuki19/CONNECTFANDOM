120394f1a0a2f8419b2b0d30b84704049a11c7de
Azuki19 <analucia27259@gmail.com>
2024-05-17 18:39:36 -0500

    feat - AppState

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,26 @@
-import { Screens } from '../types/trips';
+import { Screens } from '../types/navigation';
+import { getBands, getPosts } from '../utils/firebase';
+
+export const getPostsAction = async () => {
+	//Ir al utils de firebase y ejecutar la función getPosts
+	const posts = await getPosts();
+	return {
+		action: 'GETPOSTS',
+		payload: posts,
+	};
+};
 
 export const navigate = (screen: any) => {
 	return {
-		type: 'NAVIGATE',
+		action: 'NAVIGATE',
 		payload: screen,
+	};
+};
+
+export const getBandsAction = async () => {
+	const bands = await getBands();
+	return {
+		action: 'GETBANDS',
+		payload: bands,
 	};
 };
