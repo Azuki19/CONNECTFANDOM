c8fbf52293b013fe7b48b17592d92e5666b2880f
CrisMarin1211 <alca81411@gmail.com>
2024-06-07 11:11:38 -0500

    feat - C

--- src/components/BandasInicio/BandasInicio.ts
+++ src/components/BandasInicio/BandasInicio.ts
@@ -1,4 +1,4 @@
-import styles from './MoreAbout.css';
+import styles from './BandasInicio.css';
 import { dispatch } from '../../store';
 import { navigate } from '../../store/action';
 import { addObserver } from '../../store';
@@ -37,9 +37,6 @@ class BandasInicio extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-
-		this.onButtonClicked = this.onButtonClicked.bind(this);
-		addObserver(this);
 	}
 
 	connectedCallback() {
@@ -54,37 +51,35 @@ class BandasInicio extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-			<link rel="preconnect" href="https://fonts.googleapis.com">
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-			<link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
+            <section class='bandComponent' id='bandComponent'>
+                <img class='bandImg' src="${this.bandimage}"></img>
+                <div class='textContainer'>
+                    <h6 class='moreAbout'>More About...</h6>
+                    <div class='ArrowDiv'>
+                        <h4 class='text'>${this.bandname}</h4>
+                        <ion-icon id='chevron-forward-outline' name="chevron-forward-outline"></ion-icon>
+                    </div>
+                </div>
+            </section>
+            `;
 
-          <section class='bandComponent' id='bandComponent'>
-              <img class='bandImg' src="${this.bandimage}"></img>
-              		<div class='textContainer'>
-                		<h6 class='moreAbout'>More About...</h6>
-										<div class='ArrowDiv'>
-                    	<h4 class='text'>${this.bandname}</h4>
-											<ion-icon id='chevron-forward-outline' name="chevron-forward-outline"></ion-icon>
-											</div>
-							</div>
-
-          </section>
-          `;
+			const cssMoreAbout = this.ownerDocument.createElement('style');
+			cssMoreAbout.innerHTML = styles;
+			this.shadowRoot.appendChild(cssMoreAbout);
 		}
-		const cssMoreAbout = this.ownerDocument.createElement('style');
-		cssMoreAbout.innerHTML = styles;
-		this.shadowRoot?.appendChild(cssMoreAbout);
-	}
-	addListeners() {
-		this.shadowRoot.querySelector('.bandComponent')?.addEventListener('click', this.onButtonClicked);
 	}
 
-	onButtonClicked() {
-		console.log('holaaa');
-		dispatch(navigate('ABOUTTHEBAND'));
+	addListeners() {
+		this.shadowRoot.querySelector('.bandComponent')?.addEventListener('click', () => {
+			console.log('holaaa');
+			dispatch(navigate('ABOUTTHEBAND'));
+		});
 	}
 }
 
-customElements.define('more-about', BandasInicio);
+customElements.define('banda-info', BandasInicio);
 export default BandasInicio;
