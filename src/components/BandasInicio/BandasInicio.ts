f70da32b677b6b52170a77db489f76a59998987f
CrisMarin1211 <alca81411@gmail.com>
2024-06-07 13:57:19 -0500

    Feat - C

--- src/components/BandasInicio/BandasInicio.ts
+++ src/components/BandasInicio/BandasInicio.ts
@@ -70,22 +70,6 @@ class BandasInicio extends HTMLElement {
                     </div>
                 </section>
             `;
-=======
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-            <section class='bandComponent' id='bandComponent'>
-                <img class='bandImg' src="${this.bandimage}"></img>
-                <div class='textContainer'>
-                    <h6 class='moreAbout'>More About...</h6>
-                    <div class='ArrowDiv'>
-                        <h4 class='text'>${this.bandname}</h4>
-                        <ion-icon id='chevron-forward-outline' name="chevron-forward-outline"></ion-icon>
-                    </div>
-                </div>
-            </section>
-        `;
->>>>>>> 875692d18b33866f8ed0d860b7ada794f09bd2f7
 
 			const cssMoreAbout = this.ownerDocument.createElement('style');
 			cssMoreAbout.innerHTML = styles;
