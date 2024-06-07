4a3cf25634450b4fdc9686c10caf69893eb44ec8
CrisMarin1211 <alca81411@gmail.com>
2024-06-06 23:04:21 -0500

    feat - GETPOST - C

---
+++ src/types/postAdd.ts
@@ -0,0 +1,14 @@
+export interface PostAdd {
+	uid?: number;
+	type?: string;
+	name?: string;
+	image?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+	info?: string;
+	followers?: number;
+	titlepost?: string;
+	infopost?: string;
+	imagepost?: string;
+}
